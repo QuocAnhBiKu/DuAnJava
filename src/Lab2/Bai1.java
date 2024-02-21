f1005553b68ccc83a06887afee4836e5187ce5b2
QuocAnhBiKu <anhbr2016@gmail.com>
2024-02-21 21:39:42 +0700

    Bai 1 lab 2 xong

---
+++ src/Lab2/Bai1.java
@@ -0,0 +1,21 @@
+package Lab2;
+
+import java.util.Scanner;
+
+public class Bai1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Nhap 2 so nguyen a va b : ");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        if (a == 0 && b==0 ){
+            System.out.println("Phuong trinh vo so nghiem ");
+        } else if (a == 0 && b != 0) {
+            System.out.println("Phuong trinh vo nghiem ");
+        }else {
+            int x = (-b)/a;
+            System.out.println("Nghiem cua phuong trinh la : " + x);
+        }
+    }
+}
