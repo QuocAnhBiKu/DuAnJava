74429a74153e4ea3f7c3b7a4a57443d8a867140a
QuocAnh <anhbr2016@gmail.com>
2024-02-26 14:29:04 +0700

    Hoc oop

---
+++ src/Java/HocOOP/Student.java
@@ -0,0 +1,16 @@
+package Java.HocOOP;
+
+public class Student {
+    String name;
+    int age;
+
+    public Student(){}
+
+    public Student(String name , int age){
+        this.name = name;
+        this.age = age;
+    }
+    public void learnJava(){
+        System.out.println("Hoc java");
+    }
+}
